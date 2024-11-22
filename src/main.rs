f9130b5929a95878bb5858b9ed9d1d9491dcd731
Mohamed El Mouctar HAIDARA <elmhaidara@gmail.com>
2024-11-22 01:55:24 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct Cli {
     #[arg(short = 'H', long, default_value = "~/.zsh_history")]
     history_file: String,
 
-    /// Disable history file backup. By default, a backup is written to {history_file}.{timestamp}. Use with caution!!
+    /// Disable history file backup. By default, a backup is written to {history_file}.{timestamp} in the current directory. Use with caution!!
     #[arg(short, long, action = ArgAction::SetFalse)]
     no_backup: bool,
 }
@@ -23,22 +23,27 @@ struct Cli {
 fn main() -> ExitCode {
     let cli = Cli::parse();
 
-    match history::History::from_file(&cli.history_file) {
-        Ok(history) => {
-            println!(
-                "Found {} entries in '{}'",
-                history.entries.len(),
-                cli.history_file
-            );
-            if let Err(error) = history.write(cli.no_backup) {
-                eprintln!("{}", error);
-                return ExitCode::from(1);
-            }
-            ExitCode::from(0)
-        }
-        Err(e) => {
-            eprintln!("{}", e);
-            ExitCode::from(1)
-        }
+    if let Err(err) = run(cli) {
+        eprintln!("{}", err);
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
     }
+}
+
+fn run(cli: Cli) -> Result<(), String> {
+    let history = history::History::from_file(&cli.history_file).map_err(|err| err.to_string())?;
+
+    println!(
+        "Found {} entries in '{}'",
+        history.entries.len(),
+        cli.history_file
+    );
+
+    if cli.dry_run {
+        println!("Dry run enabled. No changes will be made.");
+        return Ok(());
+    }
+
+    history.write(cli.no_backup).map_err(|err| err.to_string())
 }
