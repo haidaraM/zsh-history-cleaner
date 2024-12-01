522768df460be2a34e009f1b44979b02b45d054d
Mohamed El Mouctar HAIDARA <elmhaidara@gmail.com>
2024-12-01 18:44:37 +0100

    cleanup

--- test_helpers/src/lib.rs
+++ test_helpers/src/lib.rs
@@ -3,13 +3,14 @@ use tempfile::NamedTempFile;
 
 pub fn get_tmp_file(commands: &str) -> NamedTempFile {
     let mut tmpfile =
-        NamedTempFile::new().expect("The tests should be able to create a temporary file!");
+        NamedTempFile::new().expect("Failed to create a temporary file for testing");
 
-    let path = tmpfile.path().to_string_lossy().to_string();
-    let msg = format!("We should able to write to the temporary file '{path}'").clone();
-    
-    let mut commands = commands.to_string();
-    commands.push('\n');
+    let msg = format!(
+        "Failed to write to the temporary file at '{}'",
+        tmpfile.path().display()
+    );
+
+    let commands = format!("{}\n", commands);
 
     tmpfile.write_all(commands.as_bytes()).expect(&msg);
 
