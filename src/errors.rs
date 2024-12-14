604c4d3ace9792fe2a53a9a2809ec0f5cc71b10b
Mohamed El Mouctar HAIDARA <elmhaidara@gmail.com>
2024-12-14 10:13:46 +0100

    upgrade packages

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,5 @@
 use thiserror::Error;
+
 #[derive(Debug, Error)]
 pub enum HistoryError {
     #[error("Failed to parse '{0}' as a history entry. Make sure this is a valid entry from a Zsh history file.")]
